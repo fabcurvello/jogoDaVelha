79af9d2be500945146f2fff9db40b279064b5c37
Fabricio Curvello <fabricio.desenvolvedor@gmail.com>
2023-09-08 17:47:00 -0300

    O jogador agora pode decidir quem inicia a partida (def quem_comeca()); O jogador escolhe a posição no tabuleiro a jogar (def jogada_escolha_usuario() ainda INCOMPLETO)

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,7 +1,6 @@
 import random
 
 # todo: REGRAS E PENDÊNCIAS A FAZER
-# todo: 1 - Perguntar ao usuário quem começa o jogo (computador ou usuário)
 # todo: 2 - Na vez do usuário, perguntar onde ele quer jogar, de acordo com as casas vazias no tabuleiro.
 # todo:   2.1 - Se ele pressionar enter ou escolher valor inválido o computador vai sortear a jogada do usuário
 # todo: 3 - Detectar quando houver um vencedor e encerrar o jogo
@@ -15,6 +14,56 @@ vez = 1
 print("-------------------------------------")
 print("--- JOGO DA VELHA --- TIC-TAC-TOE ---")
 print("-------------------------------------\n")
+
+print("-------------------------------------")
+print("---    JOGADOR    ---     PEÇA    ---")
+print("---  Computador   ---      X      ---")
+print("---     Você      ---      O      ---")
+print("-------------------------------------\n")
+
+
+def quem_comeca():
+    print("Responda quem deve iniciar a jogada:")
+    print("Digite 1 para VOCÊ iniciar;")
+    print("Digite outro número para o COMPUTADOR iniciar:")
+    starter = input()
+    global vez
+    if (starter == "1"):
+        vez = 1
+    else:
+        vez = 2
+
+
+def jogada():
+    global vez
+    if (vez == 1):  # Vez do usuário
+        input("\nSUA JOGADA: Está pronto para isso? (pressione <ENTER>)")
+        jogada_escolha_usuario()
+        # sorteio(usuario)
+        vez = 2
+    else:  # Vez do computador
+        input("\nJOGADA DO COMPUTADOR: Está pronto para isso? (pressione <ENTER>)")
+        sorteio(computador)
+        vez = 1
+
+
+def jogada_escolha_usuario():
+    exibir_tabuleiro()
+    print("Informe o número da posição no tabuleiro que você quer jogar:")
+    print("OBS: Qualquer valor inválido fará o computador sortear um número para a sua jogada.")
+    posicao = int(input())
+    tabuleiro[posicao - 1] = "O"  # todo tratar valores válidos x inválidos
+    exibir_tabuleiro()
+
+
+def sorteio(jogador):
+    sorteado = random.randint(0, 8)
+    if tabuleiro[sorteado] == " ":
+        tabuleiro[sorteado] = jogador
+        exibir_tabuleiro()
+    else:
+        print("----- MAIS UMA VEZ -----")  # todo: REMOVER FUTURAMENTE ESSA LINHA
+        sorteio(jogador)
 
 
 def exibir_tabuleiro():
@@ -31,27 +80,7 @@ def exibir_tabuleiro():
     print("         |         |         ")
 
 
-def jogada():
-    global vez
-    if (vez == 1):
-        input("\nJOGADA DO COMPUTADOR: Está pronto para isso? (pressione <ENTER>)")
-        sorteio(computador)
-        vez = 2
-    else:
-        input("\nJOGADA DO USUÁRIO: Está pronto para isso? (pressione <ENTER>)")
-        sorteio(usuario)
-        vez = 1
-
-
-def sorteio(jogador):
-    sorteado = random.randint(0, 8)
-    if tabuleiro[sorteado] == " ":
-        tabuleiro[sorteado] = jogador
-        exibir_tabuleiro()
-    else:
-        print("----- MAIS UMA VEZ -----")  # todo: REMOVER FUTURAMENTE ESSA LINHA
-        sorteio(jogador)
-
+quem_comeca()
 
 # todo: Função apenas para testes. Será melhorada conforme taferas descritas nos comentários iniciais
 while True:
