62cc02e9a63ab58814ccae5d0461052dff5add72
Fabricio Curvello <fabricio.desenvolvedor@gmail.com>
2023-09-09 21:33:30 -0300

    Tratamento de erro para o caso do usuário não inserir um número na sua jogada; Melhorra no visual do tabuleiro.

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,8 +1,6 @@
 import random
 
 # todo: REGRAS E PENDÊNCIAS A FAZER
-# todo: 2 - Na vez do usuário, perguntar onde ele quer jogar, de acordo com as casas vazias no tabuleiro.
-# todo:   2.1 - Se ele pressionar enter ou escolher valor inválido o computador vai sortear a jogada do usuário
 # todo: 3 - Detectar quando houver um vencedor e encerrar o jogo
 # todo: 4 - Detecar se o tabuleiro ficar cheio e não houver vencedor e encerrar o jogo
 
@@ -51,9 +49,21 @@ def jogada_escolha_usuario():
     exibir_tabuleiro()
     print("Informe o número da posição no tabuleiro que você quer jogar:")
     print("OBS: Qualquer valor inválido fará o computador sortear um número para a sua jogada.")
-    posicao = int(input())
-    tabuleiro[posicao - 1] = "O"  # todo tratar valores válidos x inválidos
-    exibir_tabuleiro()
+
+    try:
+        posicao = int(input())
+    except ValueError as erro:
+        posicao = 10
+
+    if( posicao > 9 or posicao < 1):
+        print("Posição escolhida é INVÁLIDA!. Sua jogada será por sorteio.....")
+        sorteio(usuario)
+    elif( tabuleiro[posicao - 1] == " "):
+        tabuleiro[posicao - 1] = "O"
+        exibir_tabuleiro()
+    else:
+        print("Posição escolhida JÁ ESTÁ OCUPADA!. Sua jogada será por sorteio.")
+        sorteio(usuario)
 
 
 def sorteio(jogador):
@@ -62,24 +72,28 @@ def sorteio(jogador):
         tabuleiro[sorteado] = jogador
         exibir_tabuleiro()
     else:
-        print("----- MAIS UMA VEZ -----")  # todo: REMOVER FUTURAMENTE ESSA LINHA
         sorteio(jogador)
 
 
 def exibir_tabuleiro():
-    print(" 1       | 2       | 3       ")
-    print(f"    {tabuleiro[0]}    |    {tabuleiro[1]}    |    {tabuleiro[2]}    ")
-    print("         |         |         ")
-    print("---------+---------+---------")
-    print(" 4       | 5       | 6       ")
-    print(f"    {tabuleiro[3]}    |    {tabuleiro[4]}    |    {tabuleiro[5]}    ")
-    print("         |         |         ")
-    print("---------+---------+---------")
-    print(" 7       | 8       | 9       ")
-    print(f"    {tabuleiro[6]}    |    {tabuleiro[7]}    |    {tabuleiro[8]}    ")
-    print("         |         |         ")
+    print("          +---------------------------------+")
+    print("          |                                 |")
+    print("          |   1       | 2       | 3         |")
+    print(f"          |      {tabuleiro[0]}    |    {tabuleiro[1]}    |    {tabuleiro[2]}      |")
+    print("          |           |         |           |")
+    print("          |  ---------+---------+---------  |")
+    print("          |   4       | 5       | 6         |")
+    print(f"          |      {tabuleiro[3]}    |    {tabuleiro[4]}    |    {tabuleiro[5]}      |")
+    print("          |           |         |           |")
+    print("          |  ---------+---------+---------  |")
+    print("          |   7       | 8       | 9         |")
+    print(f"          |      {tabuleiro[6]}    |    {tabuleiro[7]}    |    {tabuleiro[8]}      |")
+    print("          |           |         |           |")
+    print("          |                                 |")
+    print("          +---------------------------------+")
 
 
+# EXECUÇÃO DO PROJETO:
 quem_comeca()
 
 # todo: Função apenas para testes. Será melhorada conforme taferas descritas nos comentários iniciais
